d29aa49286c22bf437ece65ba374cfb0b5f644f6
rikuto-kudo <kudo1226rikuto@gmail.com>
2023-09-21 22:04:10 +0900

    8-4-課題4

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 
 class PostController extends Controller
@@ -24,7 +25,7 @@ class PostController extends Controller
         return view('posts.create');   
     }
     
-    public function store(Request $request, Post $post)
+    public function store(Post $post, PostRequest $request)
     {
         $input = $request['post'];
         $post->fill($input)->save();
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -15,19 +15,21 @@
             @csrf
             <div class="title">
                 <h2>Title</h2>
-                <input type="text" name="post[title]" placeholder="タイトル">
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}">
+                <p class="title_error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
             
             <div>
                 <h2>Body</h2>
-                <textarea name="post[body]" placeholder="本文"></textarea>
+                <textarea name="post[body]" placeholder="本文">{{ old('post.body') }}</textarea>
+                <p class="body_error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
             
             <input type="submit" value="送信">
             
         </form>
         <div class='footer'>
-            <a href='/'>戻る</a>
+            [<a href='/'>戻る</a>]
         </div>
     </body>
 </html>
