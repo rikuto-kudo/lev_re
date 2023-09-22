a484a11f476560cff0b0260fae341f56bfa0122b
rikuto-kudo <kudo1226rikuto@gmail.com>
2023-09-22 16:51:16 +0900

    8-6

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,17 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use HasFactory;
+    use SoftDeletes;
     
     Protected $fillable = [
         'title',
         'body'
     ];
+    
     public function getPaginateByLimit(int $limit_count = 5)
     {
         return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->paginate($limit_count);
     }
-    use HasFactory;
+    
 }
